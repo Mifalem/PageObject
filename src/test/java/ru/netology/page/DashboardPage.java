864d139617586243dc945a946b2bec5b224ecfba
My first test <mifalem@gmail.com>
2024-02-01 20:18:20 +0500

    Dashboard added

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,22 +1,42 @@
-package ru.netology.
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DashboardPage {
-    // к сожалению, разработчики не дали нам удобного селектора, поэтому так
-    private ElementsCollection cards = $$(".list__item div");
+
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
 
-    public Dashboard() {
+    private final SelenideElement heading = $("[data-test-id='dashboard']");
+    private final ElementsCollection cards = $$(".list__item div");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
     }
 
-    public int getFirstCardBalance() {
-        val text = cards.first().text();
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+        var text = cards.findBy(Condition.attribute("data-test-id",cardInfo.getTestId()))
+                .getText();
         return extractBalance(text);
     }
 
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(Condition.attribute("data-test-id",cardInfo.getTestId())).$("button").click();
+        return new TransferPage();
+    }
+
     private int extractBalance(String text) {
-        val start = text.indexOf(balanceStart);
-        val finish = text.indexOf(balanceFinish);
-        val value = text.substring(start + balanceStart.length(), finish);
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
         return Integer.parseInt(value);
     }
 }
