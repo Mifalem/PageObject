b765ee6ebb012df8a88604080993638849fac85c
My first test <mifalem@gmail.com>
2024-02-01 20:21:09 +0500

    Transfer added

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -1,2 +1,39 @@
-package ru.netology.page;public class TransferPage {
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private final SelenideElement transferHeader = $(byText("Пополнение карты"));
+    private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    private final SelenideElement amountInput = $("[data-test-id='amount'] input");
+    private final SelenideElement fromInput = $("[data-test-id='from'] input");
+    private final SelenideElement errorMessage = $("[data-test-id='error-notification'] .notification__content");
+
+    public TransferPage() {
+        transferHeader.shouldBe(visible);
+    }
+
+    public DashboardPage makeValidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountToTransfer, cardInfo);
+        return new DashboardPage();
+    }
+
+    public void makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+    }
+
+    public void findErrorMessage(String expectedText) {
+        errorMessage.shouldHave(text(expectedText), Duration.ofSeconds(15)).shouldBe(visible);
+    }
 }
+
